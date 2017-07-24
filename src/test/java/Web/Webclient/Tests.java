107915fe12d0029979ff889328fd456c34f65ada
svarych.a <svarych.a@novaposhta.ua>
2017-07-24 15:19:37 +0300

    24.07.2017 - 0.0.20 (try interface tests)

--- src/test/java/Web/Webclient/Tests.java
+++ src/test/java/Web/Webclient/Tests.java
@@ -18,7 +18,7 @@ public class Tests {
     @Before
     public void setBrowser() {
 
-        System.setProperty("webdriver.chrome.driver", "./chromedriver");
+        System.setProperty("webdriver.chrome.driver", "**/chromedriver");
         Configuration.browser = "chrome";
         Configuration.browserSize = "1920x1080";
         Configuration.holdBrowserOpen = true;
